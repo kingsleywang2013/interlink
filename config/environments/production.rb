88cc2f30320c64ea001e54899bf83561581b5d1c
King <kingsleywang2013@gmail.com>
2016-11-30 14:36:15 +1100

    Added SENDGRID config for authenticate confirmation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,6 +71,8 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'interlink-app.herokuapp.com', protocol: 'https' }
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
