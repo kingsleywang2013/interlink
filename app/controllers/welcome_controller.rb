e856c7d3c560461740c412bbe6e6df8567bc1446
King <kingsleywang2013@gmail.com>
2016-11-30 17:33:37 +1100

    Added home page for each user after signed in

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,9 @@
 class WelcomeController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
   def index
+    if current_user
+      redirect_to home_path
+    end
   end
 
 end
