e6b49a5261faddfb8426f15d653dd6f9c76a4bb5
King <kingsleywang2013@gmail.com>
2016-12-02 15:57:20 +1100

    Added edit profile page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -10,12 +10,28 @@ class ProfilesController < ApplicationController
       flash[:success] = "Well done, you have close to success nearly"
       redirect_to my_profile_path
     else
+      flash[:error] = "There is anything wrong when trying to create the profile"
       render 'new'
     end
   end
 
   def show
     @profile = Profile.find(params[:id])
+  end
+
+  def edit
+    @profile = Profile.find(params[:id])
+  end
+
+  def update
+    @profile = Profile.find(params[:id])
+    if @profile.update(profiles_params)
+      flash[:success] = "Your profile have been updated succssfully"
+      redirect_to my_profile_path
+    else
+      flash[:error] = "There is anything wrong when trying to update the profile"
+      render 'edit'
+    end
   end
 
   private
