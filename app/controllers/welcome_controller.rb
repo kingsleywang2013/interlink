7c92dad789d881b4b4352085613cfe4d34c11bc8
King <kingsleywang2013@gmail.com>
2016-12-02 15:46:03 +1100

    Constraint user behaviour before profile is not established anymore

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,12 @@
 class WelcomeController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
   def index
-    if current_user
+    if current_user and current_user.profile.present?
       redirect_to home_path
+
+    else
+
+      redirect_to new_profile_path
     end
   end
 
