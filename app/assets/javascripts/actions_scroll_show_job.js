cca7c9b585ff50643d1fabb200ff04e5979d24dd
King <kingsleywang2013@gmail.com>
2016-12-05 14:26:52 +1100

    Added remove and save job button on job show page, added jquery effect for buttons

---
+++ app/assets/javascripts/actions_scroll_show_job.js
@@ -0,0 +1,10 @@
+var scroll = 0;
+var marginTop = 5;
+$(document).ready(function(){
+    $(window).scroll(function () {
+        marginTop = ($(document).scrollTop() - scroll) + marginTop;
+        scroll = $(document).scrollTop();
+
+        $("#action-sidebar").animate({"marginTop": marginTop+"px"}, {duration:300,queue:false} );
+    });
+});
